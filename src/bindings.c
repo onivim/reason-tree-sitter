6f1173318627f25f7db47ef3d5379846529ec485
bryphe <bryphe@outrunlabs.com>
2019-08-12 19:42:00 -0700

    Set up finalizer for TSTree

--- src/bindings.c
+++ src/bindings.c
@@ -19,12 +19,22 @@ CAMLprim value rets_hello(value unit) {
 
 typedef struct _parser {
   TSParser *parser;
-} parser;
+} parser_W;
+
+typedef struct _tree {
+  TSTree *tree;
+} tree_W;
 
 void finalize_parser(value v) {
-  parser *p;
-  p = (parser *)Data_custom_val(v);
+  parser_W *p;
+  p = (parser_W *)Data_custom_val(v);
   ts_parser_delete(p->parser);
+}
+
+void finalize_tree(value v) {
+  tree_W *p;
+  p = (tree_W *)Data_custom_val(v);
+  ts_tree_delete(p->tree);
 }
 
 static struct custom_operations parser_custom_ops = {
@@ -36,32 +46,48 @@ static struct custom_operations parser_custom_ops = {
   deserialize : custom_deserialize_default
 };
 
+static struct custom_operations tree_custom_ops = {
+  identifier : "tree handling",
+  finalize : finalize_tree,
+  compare : custom_compare_default,
+  hash : custom_hash_default,
+  serialize : custom_serialize_default,
+  deserialize : custom_deserialize_default
+};
+
 CAMLprim value rets_parser_new_json(value unit) {
   CAMLparam0();
   CAMLlocal1(v);
-  parser parserWrapper;
+  
+  parser_W parserWrapper;
   TSParser *parser = ts_parser_new();
   parserWrapper.parser = parser;
 
-  v = caml_alloc_custom(&parser_custom_ops, sizeof(parser), 0, 1);
-  memcpy(Data_custom_val(v), &parserWrapper, sizeof(parser));
+  v = caml_alloc_custom(&parser_custom_ops, sizeof(parser_W), 0, 1);
+  memcpy(Data_custom_val(v), &parserWrapper, sizeof(parser_W));
   ts_parser_set_language(parser, tree_sitter_json());
   CAMLreturn(v);
 };
 
-CAMLprim value rets_parser_parse_string(value vParser, value source) {
-  CAMLparam2(vParser, source);
-  parser *p = Data_custom_val(vParser);
+CAMLprim value rets_parser_parse_string(value vParser, value vSource) {
+  CAMLparam2(vParser, vSource);
+  CAMLlocal1(v);
+  
+  parser_W *p = Data_custom_val(vParser);
   TSParser *tsparser = p->parser;
 
-  const char *source_code = "[1, null]";
+  const char *source_code = String_val(vSource);
   TSTree *tree =
       ts_parser_parse_string(tsparser, NULL, source_code, strlen(source_code));
 
+  tree_W treeWrapper;
+  treeWrapper.tree = tree;
+  
   TSNode node = ts_tree_root_node(tree);
   char *string = ts_node_string(node);
   printf("Syntax tree: %s\n", string);
 
-  printf("Parse called\n");
-  CAMLreturn(Val_unit);
+  v = caml_alloc_custom(&tree_custom_ops, sizeof(tree_W), 0, 1);
+  memcpy(Data_custom_val(v), &treeWrapper, sizeof(tree_W));
+  CAMLreturn(v);
 };
--- test/ParserTest.re
+++ test/ParserTest.re
@@ -5,10 +5,21 @@ open Treesitter;
 describe("Parser", ({describe, _}) => {
   describe("allocation", ({test, _}) => {
 
-    test("finalizer gets called", ({expect, _}) => {
+    test("finalizer gets called for parser", ({expect, _}) => {
       let jsonParser = Parser.json();
       let callCount = ref(0);
       Gc.finalise_last(() => incr(callCount), jsonParser);
+      Gc.full_major();
+
+      expect.int(callCount^).toBe(1);
+    });
+    
+    test("finalizer gets called for tree", ({expect, _}) => {
+      let jsonParser = Parser.json();
+      let tree = Parser.parseString(jsonParser, "[1, \"a\", null]");
+      
+      let callCount = ref(0);
+      Gc.finalise_last(() => incr(callCount), tree);
       Gc.full_major();
 
       expect.int(callCount^).toBe(1);
@@ -18,7 +29,7 @@ describe("Parser", ({describe, _}) => {
   describe("json", ({test, _}) => {
     test("finalizer gets called", ({expect, _}) => {
       let jsonParser = Parser.json();
-      let _ = Parser.parseString(jsonParser, "[1, null]");
+      let _ = Parser.parseString(jsonParser, "[1, \"a\", null]");
 
       // TODO:
       expect.int(1).toBe(1);
